3fd202adacac45189b52250e51453af6913eefaf
Scott Siegel <scottmatthewsiegel@gmail.com>
2021-06-22 19:20:54 -0500

    redid file structure to match recommendation in assignments readme. created first html template in src

---
+++ index.js
@@ -0,0 +1,10 @@
+const Employee = require("./lib/employee");
+const Manager = require("./lib/manager");
+const Engineer = require("./lib/engineer");
+const Intern = require("./lib/intern")
+const generate = require("./lib/htmlGenerator")
+const inquirer = require("inquirer");
+const path = require("path");
+const fs = require("fs");
+const OUTPUT_DIR = path.resolve(__dirname, "output");
+const outputPath = path.join(OUTPUT_DIR, "team.html");
