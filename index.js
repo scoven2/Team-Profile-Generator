fe3516e40606fe4e1e889b5e6bdce9a625c12741
Scott Siegel <scottmatthewsiegel@gmail.com>
2021-09-29 20:14:47 -0500

    added source from class that work is based on and removed visual aid html

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-//entire project based on unit 10 activities 24, 25, 26, and 28
-
+// based on unit 9 activity 28
+// const inquirer = require('inquirer');
+// const fs = require('fs');
+// const util = require('util');
 const inquirer = require("inquirer");
 const Server = require("./Lib/Server");
 const Staff = require("./Lib/Staff");
@@ -7,6 +9,41 @@ const Supervisor = require("./Lib/Supervisor");
 const Host = require("./Lib/Host");
 const fs = require("fs");
 
+// based on unit 9 activity 28
+// const promptUser = () => {
+//     return inquirer.prompt([
+//       {
+//         type: 'input',
+//         name: 'name',
+//         message: 'What is your name?',
+//       },
+//       {
+//         type: 'input',
+//         name: 'location',
+//         message: 'Where are you from?',
+//       },
+//       {
+//         type: 'input',
+//         name: 'hobby',
+//         message: 'What is your favorite hobby?',
+//       },
+//       {
+//         type: 'input',
+//         name: 'food',
+//         message: 'What is your favorite food?',
+//       },
+//       {
+//         type: 'input',
+//         name: 'github',
+//         message: 'Enter your GitHub Username',
+//       },
+//       {
+//         type: 'input',
+//         name: 'linkedin',
+//         message: 'Enter your LinkedIn URL.',
+//       },
+//     ]);
+//   };
 function runInquirer() {
     const promptArray = [{
         type: "input",
@@ -60,11 +97,40 @@ function runInquirerSupervisor() {
         .prompt(promptArray);
 }
 
+        //unit 10 activity 7 and 28
+        //7
+        // const willGetSwitch = new Promise((resolve, reject) => {
+        //     if (choresDone) {
+        //         const reward = {
+        //             name: 'Nintendo Switch',
+        //             desired: true,
+        //         };
+        //         resolve(reward);
+        //28:
+        // makeGuess() {
+        //     this.askForLetter().then(() => {
+        //       if (this.guessesLeft < 1) {
+        //         console.log(
+        //           'No guesses left! Word was: "' +
+        //             this.currentWord.getSolution() +
+        //             '"\n'
+        //         );
+        //         this.askToPlayAgain();
+        //       } else if (this.currentWord.guessedCorrectly()) {
+        //         console.log("You got it right! Next word!");
+        //         this.guessesLeft = 10;
+        //         this.nextWord();
+        //       } else {
+        //         this.makeGuess();
+        //       }
+        //     });
+        //   }
 async function run() {
     let staffArray = [];
     const maxTimes = 4;
     for (i = 0; i < maxTimes; i++) {
         const promise = new Promise((resolve, reject) => {
+
             runInquirer()
                 .then(function({ name, id, phone, position }) {
                     if (position === "Supervisor") {
@@ -111,7 +177,30 @@ async function run() {
             return `Discord: ${staff.discord}`;
         }
     }
-
+//based on unit 9 activity 28
+// const generateHTML = (answers) =>
+//     `<!DOCTYPE html>
+// <html lang="en">
+// <head>
+//   <meta charset="UTF-8">
+//   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+//   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+//   <title>Document</title>
+// </head>
+// <body>
+//   <div class="jumbotron jumbotron-fluid">
+//   <div class="container">
+//     <h1 class="display-4">Hi! My name is ${answers.name}</h1>
+//     <p class="lead">I am from ${answers.location}.</p>
+//     <h3>Example heading <span class="badge badge-secondary">Contact Me</span></h3>
+//     <ul class="list-group">
+//       <li class="list-group-item">My GitHub username is ${answers.github}</li>
+//       <li class="list-group-item">LinkedIn: ${answers.linkedin}</li>
+//     </ul>
+//   </div>
+// </div>
+// </body>
+// </html>`;
     function getCardHtml() {
         let html = "";
         for (j = 0; j < maxTimes; j++) {
@@ -151,6 +240,9 @@ async function run() {
         `;
     console.log(html);
     const fs = require("fs");
+//based on unit 9 activity 28
+// fs.writeFile('index.html', htmlPageContent, (err) =>
+// err ? console.log(err) : console.log('Successfully created index.html!')
     fs.writeFile('teamProfile.html', html, function(err) {
         if (err) throw err;
         console.log('File created!');
